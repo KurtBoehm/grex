eff85de073bd3990817f100e052509517ebc9f18
Kurt Böhm <kurbo96@gmail.com>
2025-06-03 19:05:40 +0200

    Test `cutoff` and `cutoff_mask`

--- test/set.cpp
+++ test/set.cpp
@@ -19,6 +19,7 @@ template<grex::Vectorizable T, std::size_t tSize>
 void run(grex::TypeTag<T> /*tag*/, grex::IndexTag<tSize> /*tag*/) {
   using VC = test::VectorChecker<T, tSize>;
   using MC = test::MaskChecker<T, tSize>;
+  using Mask = grex::Mask<T, tSize>;
 
   // vector
   {
@@ -30,25 +31,25 @@ void run(grex::TypeTag<T> /*tag*/, grex::IndexTag<tSize> /*tag*/) {
     checker.check();
   }
   {
-    grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
-      VC checker{T(tSize - tIdxs)...};
-      checker.check();
-    });
-  }
-  {
     auto checker = VC::indices();
     checker.check();
   }
-  {
-    grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
-      const VC base{T(T(tSize) - 2 * T(tIdxs) + 1)...};
-      for (std::size_t i = 0; i < tSize; ++i) {
-        const auto val = T((T(i % 7) - T(2)) * T(3));
-        VC v{base.vec.insert(i, val), std::array{((tIdxs == i) ? val : base.ref[tIdxs])...}};
-        v.check();
-      }
-    });
-  }
+  grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
+    VC checker{T(tSize - tIdxs)...};
+    checker.check();
+  });
+  grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
+    const VC base{T(T(tSize) - 2 * T(tIdxs) + 1)...};
+    for (std::size_t i = 0; i < tSize; ++i) {
+      const auto val = T((T(i % 7) - T(2)) * T(3));
+      VC v{base.vec.insert(i, val), std::array{((tIdxs == i) ? val : base.ref[tIdxs])...}};
+      v.check();
+    }
+    for (std::size_t i = 0; i <= tSize; ++i) {
+      VC v{base.vec.cutoff(i), std::array{((tIdxs < i) ? base.ref[tIdxs] : T(0))...}};
+      v.check();
+    }
+  });
 
   // mask
   {
@@ -67,22 +68,24 @@ void run(grex::TypeTag<T> /*tag*/, grex::IndexTag<tSize> /*tag*/) {
     MC checker{true};
     checker.check();
   }
-  {
-    grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
-      MC checker{((tIdxs % 5) % 2 == 0)...};
-      checker.check();
-    });
-  }
-  {
-    grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
-      const MC base{(tIdxs % 3 != 1)...};
-      for (std::size_t i = 0; i < tSize; ++i) {
-        const bool val = i % 5 == 0;
-        MC v{base.mask.insert(i, val), std::array{((tIdxs == i) ? val : base.ref[tIdxs])...}};
-        v.check();
-      }
-    });
-  }
+  grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
+    MC checker{((tIdxs % 5) % 2 == 0)...};
+    checker.check();
+  });
+  grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
+    const MC base{(tIdxs % 3 != 1)...};
+    for (std::size_t i = 0; i < tSize; ++i) {
+      const bool val = i % 5 == 0;
+      MC v{base.mask.insert(i, val), std::array{((tIdxs == i) ? val : base.ref[tIdxs])...}};
+      v.check();
+    }
+  });
+  grex::static_apply<tSize>([]<std::size_t... tIdxs>() {
+    for (std::size_t i = 0; i <= tSize; ++i) {
+      MC v{Mask::cutoff_mask(i), std::array{(tIdxs < i)...}};
+      v.check();
+    }
+  });
 }
 
 int main() {
