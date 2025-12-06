01265eae2cd860a5e439aef8edbd779a0c1bb545
Kurt Böhm <kurbo96@gmail.com>
2025-12-06 16:51:25 +0100

    Fix a small issue with the shared `shrink` header

--- include/grex/backend/shared/operations/shrink.hpp
+++ include/grex/backend/shared/operations/shrink.hpp
@@ -9,9 +9,9 @@
 
 #include <cstddef>
 
+#include "grex/backend/active/sizes.hpp"
 #include "grex/backend/choosers.hpp"
 #include "grex/backend/defs.hpp"
-#include "grex/backend/neon/sizes.hpp"
 #include "grex/base.hpp"
 
 namespace grex::backend {
